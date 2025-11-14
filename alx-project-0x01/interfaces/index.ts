c01ee9ee1e81bef4aed08d202651f3cc174cac5d
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-14 19:04:24 +0100

    Update index.ts

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -27,4 +27,15 @@ export interface UserProps {
   website: string;
   company: Company;
 }
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
 
