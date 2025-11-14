c8130d4e37d6e09f8800ec0b58d4ab7b23950b30
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-14 19:01:01 +0100

    Update index.ts

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,8 +1,20 @@
-export interface PostProps {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
+export interface Geo {
+  lat: string;
+  lng: string;
+}
+
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
 }
 
 export interface UserProps {
@@ -10,11 +22,9 @@ export interface UserProps {
   name: string;
   username: string;
   email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
 
